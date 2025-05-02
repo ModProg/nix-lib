6699bd5c2d684224e7a729f98f0e4555edb9ad06
hi <hi>
2025-05-02 16:48:25 +0200

    fixFilterMap

--- src/lists.nix
+++ src/lists.nix
@@ -3,11 +3,11 @@ rec {
   push = list: elem: list ++ [ elem ];
   filterMap =
     fn:
-    builtins.foldl' [ ] (
+    builtins.foldl' (
       acc: elem:
       let
         value = fn elem;
       in
       if value == null then acc else push acc elem
-    );
+    ) [ ];
 }
